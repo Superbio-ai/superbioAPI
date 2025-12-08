bb086c81f86c19296848bc28c19d991d228bf0c3
drogo <dmason@superbio.ai>
2025-12-08 12:16:35 +0000

    force update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="superbio",
-    version="0.1.6",
+    version="0.1.7",
     author="Superbio",
     author_email="dmason@superbio.ai",
     description="Python client for the Superbio API",
